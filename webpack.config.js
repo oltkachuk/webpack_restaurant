2375f1c912ae751dac7dc99531e1ee014b9534d1
Oleh Tkachuk <olezuktkachuk22@gmial.com>
2020-09-08 13:52:04 +0300

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist')    
+    },
+    devServer: {
+        port: 4200
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+            chunkFilename: '[id].css'
+        })
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {},
+                },
+                'css-loader'
+            ],
+          },
+          {
+            test: /\.(jpg|svg|png)$/,
+            use: [
+                'file-loader'
+            ],
+          }
+        ],
+    }
+}
